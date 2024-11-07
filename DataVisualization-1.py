cc6c1415b673fb2999b3a175b7401aaa4fa326de
Sriveni Kunduru <srivenibhaskar@gmail.com>
2024-11-07 10:45:33 -0500

    Data Visualization for Sentiment Distribution by Summary Topics and document topics

---
+++ DataVisualization-1.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+
+from google.colab import drive
+drive.mount('/content/drive')
+
+
+# In[3]:
+
+
+df = pd.read_csv('/content/drive/MyDrive/Project/text_analysis.csv')
+
+df.head(2)
+
+
+# In[4]:
+
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Count the occurrences of each sentiment per topic for document and summary
+doc_sentiment_distribution = df.groupby(['document_topics', 'document_sentiment_category']).size().unstack(fill_value=0)
+summary_sentiment_distribution = df.groupby(['summary_topics', 'summary_sentiment_category']).size().unstack(fill_value=0)
+
+# Setting up the plot with two subplots
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(16, 6))
+
+# Plot for document topics
+doc_sentiment_distribution.plot(kind='bar', stacked=True, colormap='coolwarm', edgecolor='black', ax=axes[0])
+axes[0].set_title('Sentiment Distribution by Document Topics', fontsize=16, fontweight='bold')
+axes[0].set_xlabel('Topics', fontsize=12)
+axes[0].set_ylabel('Number of Documents', fontsize=12)
+axes[0].set_xticklabels(axes[0].get_xticklabels(), rotation=45, ha='right', fontsize=10)
+axes[0].legend(title='Sentiment', title_fontsize='13', fontsize='10')
+
+# Plot for summary topics
+summary_sentiment_distribution.plot(kind='bar', stacked=True, colormap='coolwarm', edgecolor='black', ax=axes[1])
+axes[1].set_title('Sentiment Distribution by Summary Topics', fontsize=16, fontweight='bold')
+axes[1].set_xlabel('Topics', fontsize=12)
+axes[1].set_ylabel('Number of Summaries', fontsize=12)
+axes[1].set_xticklabels(axes[1].get_xticklabels(), rotation=45, ha='right', fontsize=10)
+axes[1].legend(title='Sentiment', title_fontsize='13', fontsize='10')
+
+# Styling for professional look
+sns.set_style("whitegrid")
+plt.grid(True, linestyle='--', alpha=0.6)
+
+# Adjust layout for better spacing
+plt.tight_layout()
+plt.show()
+
+
+# **Insight :**
+# 
+# 1) In both charts, certain topics (like - "one, like, time, feel") are much more prevalent in terms of the number of documents or summaries, and they tend to carry a significant amount of Negative sentiment.
+# 
+# 2) Neutral sentiment appears less frequent in general compared to Negative and Positive sentiments across both documents and summaries.
+
+# In[ ]:
+
+
+
+
